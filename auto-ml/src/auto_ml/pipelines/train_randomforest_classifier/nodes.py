10481cd75f5466cbd408fccf165c925074a41d56
john-adeojo <johnadeojo91@gmail.com>
2023-07-07 00:23:43 +0100

    Create Pipelines

---
+++ auto-ml/src/auto_ml/pipelines/train_randomforest_classifier/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'train_randomforest_classifier'
+generated using Kedro 0.18.11
+"""
--- pipelines.bat
+++ pipelines.bat
@@ -1,4 +1,7 @@
+@echo off
+call conda activate automl-env
 kedro pipeline create dataprocessing
 kedro pipeline create train_xgboost_classifier
 kedro pipeline create train_randomforest_classifier
 kedro pipeline create train_ridge_classifier
+kedro pipeline create model_diagnostics
